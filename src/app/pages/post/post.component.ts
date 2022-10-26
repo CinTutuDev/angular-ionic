be83376b89dc9b3821cf1bdf6db08349bf80294f
cinta <cinta.tafur@galvintec.com>
2022-10-26 13:10:50 +0200

    🥇muestro JSON por ngFor

--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -9,13 +9,19 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  /* creo un arrau vacio para llamarlo y coger*/
+  mensaje: any[] =[];
   /* inyecto el sercicio para coger la peticion del data.service */
   constructor( private dataService: DataService) { }
 
-  ngOnInit(){
 
-    this.dataService.getPosts().subscribe(posts => {
+
+  ngOnInit(){
+    /* con el metodo getPosts recojo los datos de la url los miuestro por consola */
+    this.dataService.getPosts()
+    .subscribe((posts: any) => {
       console.log(posts);
+      this.mensaje = posts;
     });
     
   }
