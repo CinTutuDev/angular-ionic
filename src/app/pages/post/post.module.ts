83b00f6ad75e638841ae4a57afc7555be871fcd1
cinta <cinta.tafur@galvintec.com>
2022-10-26 14:47:16 +0200

    🐞Recuepero algo de cosas

--- src/app/pages/post/post.module.ts
+++ src/app/pages/post/post.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { PostRoutingModule } from './post-routing.module';
 import { PostComponent } from './post.component';
+/* lo puse en pllural aunque es un post por error */
+import { PostsComponent } from './posts/posts.component';
 
 
 @NgModule({
   declarations: [
-    PostComponent
+    PostComponent,
+    PostsComponent
   ],
   imports: [
     CommonModule,
